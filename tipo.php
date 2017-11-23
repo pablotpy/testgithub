553c5117d2ad84507282c8893522d76b9720750e
Unknown <pablolpdt@gmail.com>
2017-11-22 21:25:13 -0300

    volviendo a su estado original
    testeo de push

---
+++ tipo.php
@@ -0,0 +1,15 @@
+<?php
+
+$entero = -150;
+$decimal = 150.55;
+$cadena = 'soy un steamer';
+$bool =false;
+$bool =true;
+
+
+echo "Valor Decimal: $decimal";
+echo "<br>Valor Numerico: $entero";
+echo "<br>Valor de tipo String: $cadena";
+
+
+?>
